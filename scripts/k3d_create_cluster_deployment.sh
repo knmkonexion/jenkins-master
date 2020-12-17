a6680988cb25bbfea453bbc11e276d9c61726e50
Kasey Weirich <knmkonexion@gmail.com>
2020-12-16 18:55:13 -0700

    add notes to script

--- scripts/k3d_create_cluster_deployment.sh
+++ scripts/k3d_create_cluster_deployment.sh
@@ -9,12 +9,18 @@ clear
 #----------
 # WORKING
 #----------
+# NOTES: Deploys to namespace from single file
+# can break the namespace yaml up if desired...will just need to chagne namespaces to deploy content(s)
+#----------
 # k3d cluster delete web-cluster
 # k3d cluster create web-cluster --api-port 6551 -p "8090:80@loadbalancer" --agents 1
 # kubectl apply -f k8s/dev-nginx.yaml
 
 #----------
 # WORKING
+#----------
+# NOTES: Deploys Jenkins with persistent/node/cluster mount points
+# this is for local development only, not a good habit for prod workloads
 #----------
 k3d cluster delete jenkins
 k3d cluster create jenkins --api-port 6552 -p "8082:80@loadbalancer" -v /mnt/d/k8s_datastore:/k3d_datastore -v /var/run/docker.sock:/var/run/docker.sock --agents 1
